e55d1bb7c872dd483356e52ea771bb13f93e0084
Rafal Kordas <rafalkordas063@gmail.com>
2018-04-11 10:20:47 +0200

    Add interface Shuflleable, add deal method in Deck.java, change import in Deck

--- src/main/java/com/codecool/battleofcards/card/Deck.java
+++ src/main/java/com/codecool/battleofcards/card/Deck.java
@@ -1,9 +1,9 @@
-package main.java.com.codecool.battleofcards.card;
+package com.codecool.battleofcards.card;
 
-import java.util.Collections;
+import java.util.*;
 import java.io.*;
 
-public class Deck{
+public class Deck implements Shuffleable{
 
     private List<Card> cards;
     private String fileContent = readerFromFile();
@@ -25,13 +25,13 @@ public class Deck{
                 CardAttribute firstAttribute = new CardAttribute("Population density (person per km)", firstValue);
 
                 double secondValue = Double.valueOf(cardStatistic[2]);
-                CardAttribute secondAttribute = new CardAttribute("Population size ()", firstValue);
+                CardAttribute secondAttribute = new CardAttribute("Population size", secondValue);
                 
-                double thirdtValue = Double.valueOf(cardStatistic[3]);
-                CardAttribute thirdAttribute = new CardAttribute("GDP (Euro)", firstValue);
+                double thirdValue = Double.valueOf(cardStatistic[3]);
+                CardAttribute thirdAttribute = new CardAttribute("GDP (EU)", thirdValue);
 
                 double fourthValue = Double.valueOf(cardStatistic[4]);
-                CardAttribute fourthAttribute = new CardAttribute("Area (square km)", firstValue);
+                CardAttribute fourthAttribute = new CardAttribute("Area (square km)", fourthValue);
 
                 CardAttributes[] attributes = {firstAttribute, secondAttribute, thirdAttribute, fourthAttribute};
 
@@ -43,8 +43,18 @@ public class Deck{
         Collections.shuffle(this.cards);
     }
 
-    public List<Card> deal(int num){
-        // TODO
+    public List<Pile> deal(int num){
+        List<Pile> piles = new ArrayList<>();
+
+        int start = 0;
+        int numPlayer = this.cards.size() / num;
+
+        for(int i = 0; i<num; i++){
+            piles.add(new Pile(this.cards.sublist(start,numPlayer)));
+            start = numPlayer;
+            numPlayer += start;
+        }
+        return piles;
     }
     
     public String readerFromFile() {
