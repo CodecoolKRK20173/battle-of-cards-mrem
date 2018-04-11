de1951f10b34a38a7b419222885de14b339680b5
Rafal Kordas <rafalkordas063@gmail.com>
2018-04-11 13:33:08 +0200

    Add Game constructor, add checkIfWon method

---
+++ src/main/java/com/codecool/battleofcards/game/Game.java
@@ -0,0 +1,51 @@
+package com.codecool.battleofcards.game;
+
+import com.codecool.battleofcards.player.*;
+import com.codecool.battleofcards.display.*;
+
+import java.util.List;
+
+public class Game{
+
+    private List<Player> players;
+    private Player activePlayer;
+    private Table table;
+    private boolean isActive;
+    private GameView gameView;
+
+    public Game(List<Player> players){
+        this.players = players;
+        this.table = new Table();
+        this.gameView = new GameView();
+        this.isActive = true;
+    
+    }
+
+    private void play(){
+
+    }
+
+    private void handleRound(){
+
+    }
+
+    private void showCard(Player player){
+
+    }
+
+    private void showCards(List<Player>){
+
+    }
+
+    private void checkIfWon(){
+        for(Player player : players){
+            if(player.getCards.isEmpty()){
+                this.isActive = false;
+            }
+        }
+    }
+
+    private Player findPlayer(){
+
+    }
+}
