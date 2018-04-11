83412f6f62af25ab128ee154c38026245e337773
Elzbieta Krzych <elzbieta.krzych@gmail.com>
2018-04-11 12:14:18 +0200

    Create basic methods in GameInitializer

---
+++ src/main/java/com/codecool/battleofcards/game/GameInitializer.java
@@ -0,0 +1,15 @@
+package com.codecool.battleofcards.game;
+
+class GameInitializer {
+    private Deck deck;
+    private List<Player> player;
+
+    public GameInitializer () {}
+
+    public void initializeGame() {}
+
+    private void createDeck() {} //"statistics.csv"
+    private void createPlayers() {} // String Pile
+    private void dealCards() {}
+
+}
