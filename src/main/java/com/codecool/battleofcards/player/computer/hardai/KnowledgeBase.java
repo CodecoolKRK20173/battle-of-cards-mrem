9e8217ba86e79edf4dbb90bebb9a0eff6b73dcbc
kret0 <mjp.sik@gmail.com>
2018-04-13 00:15:56 +0200

    add methods to KnowledgeBase allowing to decide attributes strenghts for each card in deck

--- src/main/java/com/codecool/battleofcards/player/computer/hardai/KnowledgeBase.java
+++ src/main/java/com/codecool/battleofcards/player/computer/hardai/KnowledgeBase.java
@@ -13,6 +13,11 @@ public class KnowledgeBase{
             initializeBase(); // ... we want to be sure we will not run this more than once
         }
     }
+
+    public int getBestAttributeOfCard(Card card){
+        return cardsStrenghts.get(card).getBestAttributeNumber();
+    }
+
     private void initializeBase(){
         comparatorFactory = new CardComparatorFactory();
         cardsStrenghts = new HashMap<Card, CardStrenghts>();
@@ -27,4 +32,26 @@ public class KnowledgeBase{
         for (int i = 1; i <= CARD_STATS_NUM; i++)
             generateAttributeStrenghts(i, allCards);
 
+    }
+
+
+    private void initializeCardsStrenghts(List<Card> cards){
+
+        for(Card card : cards)
+            cardsStrenghts.put(card, new CardStrenghts());
+    //    deckSize = cards.size();
+
+    }
+
+    private void generateAttributeStrenghts(int attributeNumber, List<Card> cards){
+
+        Collections.sort(cards, comparatorFactory.getCardComparator(attributeNumber));
+
+        for(int i = 0; i < cards.size(); i++){
+            cardsStrenghts.get(cards.get(i)).setAttribute(attributeNumber, (i));
+        }
+    }
+
+    private double calculatePercentage(int chunk, int size){
+        return ((size - (chunk+1)) * 100) / size;
     }
