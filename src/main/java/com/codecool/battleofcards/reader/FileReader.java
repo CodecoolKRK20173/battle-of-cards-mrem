56cf965bf8f19853bc2a874be0b760a47e364243
kret0 <mjp.sik@gmail.com>
2018-04-13 01:29:38 +0200

    Fill java doc for FileReader.java

--- src/main/java/com/codecool/battleofcards/reader/FileReader.java
+++ src/main/java/com/codecool/battleofcards/reader/FileReader.java
@@ -5,9 +5,25 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+
+    /**
+    * This class is used to upload Cards data from given csv file.
+    **/
+
 public class FileReader {
+
+    /*
+    * This method upload Cards data from given csv file
+    * <p>This method throws a <tt>IOException</tt> if given path doesn't exist.
+    *
+    * @param filename name of file to open.
+    * @return Card data as string
+    * @throws IOException if given path doesn't exist.
+    */
+
     public String readerFromFile(String filename) {
         StringBuilder fileContent = new StringBuilder();
+
 
         ClassLoader classLoader = getClass().getClassLoader();
         Path filePath = Paths.get(classLoader.getResource(filename).getFile());
