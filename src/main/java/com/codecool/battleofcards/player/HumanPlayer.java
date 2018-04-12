5c512fd03afb972efbf0ed8ac92608fe6dc50e64
Marcin Tuleja <marcintuleja@gmail.com>
2018-04-12 23:02:35 +0200

    Add exception handling for player choice of the card attribute to compare

--- src/main/java/com/codecool/battleofcards/player/HumanPlayer.java
+++ src/main/java/com/codecool/battleofcards/player/HumanPlayer.java
@@ -1,7 +1,6 @@
 package com.codecool.battleofcards.player;
 
 import com.codecool.battleofcards.card.Pile;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class HumanPlayer extends Player {
@@ -11,16 +10,13 @@ public class HumanPlayer extends Player {
     
     @Override
     public int getChoice() {
-        try {
-            Scanner reader = new Scanner(System.in);
-            int userChoice = Integer.parseInt(reader.nextLine());
-            if (userChoice < 1 || userChoice > 4) {
-                throw new InputMismatchException("It's not a valid number");
-            }
-            return userChoice;
-        } catch (NumberFormatException e) {
-            System.err.println("It's not a number");
+        Scanner reader = new Scanner(System.in);
+        int userChoice = Integer.parseInt(reader.nextLine());
+
+        if (userChoice < 1 || userChoice > 4) {
+            throw new IllegalArgumentException("It's not a valid number");
         }
-        return -1;
+
+        return userChoice;
     }
 }
