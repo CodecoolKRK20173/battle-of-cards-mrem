227844bf3c16d040f5add21ad358031a7e5a4187
kret0 <mjp.sik@gmail.com>
2018-04-13 02:57:22 +0200

    Fill javadocs in comparator package

--- src/main/java/com/codecool/battleofcards/card/comparator/CardComparator.java
+++ src/main/java/com/codecool/battleofcards/card/comparator/CardComparator.java
@@ -3,13 +3,27 @@ package com.codecool.battleofcards.card.comparator;
 import com.codecool.battleofcards.card.Card;
 import java.util.Comparator;
 
+/**
+* This class is used to compare Card objects by given attribute nuber.
+*/
 public class CardComparator implements Comparator<Card> {
     private final int attributeNumber;
 
+    /**
+     * Constructor of CardComparator.
+     * <p>Recives attributeNumber.
+     * @param  attributeNumber number of attribute of Card object.
+     */
     public CardComparator(int attributeNumber) {
         this.attributeNumber = attributeNumber;
     }
 
+    /**
+     * Compares cardAttribute of Card object.
+     * @param  card      card to compare
+     * @param  otherCard to compare with
+     * @return           1 if card > other, -1 if card < other , 0 if equals
+     */
     @Override
     public int compare(Card card, Card otherCard) {
         if (card.getAttributeValue(this.attributeNumber) > otherCard.getAttributeValue(this.attributeNumber)) {
--- src/main/java/com/codecool/battleofcards/card/comparator/CardComparatorFactory.java
+++ src/main/java/com/codecool/battleofcards/card/comparator/CardComparatorFactory.java
@@ -6,12 +6,26 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.Comparator;
 
+/**
+* This class is a factory for Card comparators.
+* <p> @see CardComparator
+*/
 public class CardComparatorFactory {
     private final Map<Integer, Comparator<Card>> comparators;
 
+    /**
+     * CardComparatorFactory constructor
+     */
     public CardComparatorFactory() {
         this.comparators = new HashMap<>();
     }
+
+    /**
+    * Method creating comparators on the go.
+    * <p> Uses flyweight pattern to create new comparators
+    * @param attributeNumber number of attribute of Card object.
+    * @return comparator from given attributeNumber
+    */
 
     public Comparator<Card> getCardComparator(int attributeNumber) {
         if (!this.comparators.containsKey(attributeNumber)) {
